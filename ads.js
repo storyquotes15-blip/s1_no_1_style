d33f9b7d3e2e4a28766828239efcda3ce61de8d2
dats <storyquotes15@gmail.com>
2026-06-28 23:03:59 +0800

    Add files via upload

---
+++ ads.js
@@ -0,0 +1,21 @@
+const ADS = [
+
+{
+title:"🎬 Cinema Gold PRO",
+image:"ads/cinemagold.jpg",
+url:"https://lynk.id/temablogger/eglqroq9j4jm"
+},
+
+{
+title:"BukaLink Blogger Template",
+image:"ads/temabloger.jpg",
+url:"https://lynk.id/temablogger/zolv5jvjq51x"
+},
+
+{
+title:"🚀 Comic Ads Vol.1 PRO",
+image:"ads/comicads.jpg",
+url:"https://lynk.id/temablogger/qo7rdyxmv1qm"
+},
+
+];
